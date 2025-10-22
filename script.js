0adeb3bc331c05f5ef93e4c8695f0b425aa0ff8a
gabriel <celestinogabriel1441@gmail.com>
2025-10-22 11:07:28 -0300

    lógica base do projeto

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-
+window.document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById("btnMaiuscula").addEventListener("click",
+        function(){
+            let input = document.querySelector("#inputUser").value
+            document.querySelector("#resultado").innerHTML = "Resultado: " + input.toUpperCase()
+        })
+    document.getElementById("btnMinuscula").addEventListener("click",
+        function(){
+            let input = document.querySelector("#inputUser").value
+            document.querySelector("#resultado").innerHTML = "Resultado: " + input.toLowerCase()
+        })
+        
+})
 
